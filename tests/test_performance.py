Fix returns calculation and unittests.

tests/test_performance.py
@@ -15,9 +15,11 @@ def test_annualized_returns(single_prices):
 def test_annualized_standard_deviation(single_prices):
     s1 = 365
     s2 = 252
-    r = single_prices / single_prices.shift(1)
-    assert np.std(r) * np.sqrt(s1) == PerformanceStatistics().annualized_standard_deviation(single_prices, s1)
-    assert np.std(r) * np.sqrt(s2) == PerformanceStatistics().annualized_standard_deviation(single_prices, s2)
+    r = single_prices.diff(1) / single_prices.shift(1)
+    assert round(np.std(r) * np.sqrt(s1), 10) == round(
+        PerformanceStatistics().annualized_standard_deviation(single_prices, s1), 10)
+    assert round(np.std(r) * np.sqrt(s2), 10) == round(
+        PerformanceStatistics().annualized_standard_deviation(single_prices, s2), 10)
 
 
 def test_maximum_daily_drawdown(single_prices):
@@ -36,13 +38,11 @@ def test_information_ratio(single_prices):
     s1 = 365
     s2 = 252
     ar = single_prices.iloc[-1] / single_prices.iloc[0]
-    r = single_prices / single_prices.shift(1)
-    assert (ar ** (s1 / len(single_prices)) - 1) / (
-            np.std(r) * np.sqrt(s1)) == PerformanceStatistics().information_ratio(
-        single_prices, s1)
-    assert (ar ** (s2 / len(single_prices)) - 1) / (
-            np.std(r) * np.sqrt(s2)) == PerformanceStatistics().information_ratio(
-        single_prices, s2)
+    r = single_prices.diff(1) / single_prices.shift(1)
+    assert round((ar ** (s1 / len(single_prices)) - 1) / (np.std(r) * np.sqrt(s1)), 10) == round(
+        PerformanceStatistics().information_ratio(single_prices, s1), 10)
+    assert round((ar ** (s2 / len(single_prices)) - 1) / (np.std(r) * np.sqrt(s2)), 10) == round(
+        PerformanceStatistics().information_ratio(single_prices, s2), 10)
 
 
 def test_loss_duration(single_prices):
@@ -74,7 +74,8 @@ def test_max_loss_duration():
 
 
 def test_calculate_r(single_prices):
-    assert PerformanceStatistics._calculate_r(single_prices).equals(single_prices / single_prices.shift(1))
+    pd.testing.assert_series_equal(PerformanceStatistics._calculate_r(single_prices),
+                                   single_prices.diff(1) / single_prices.shift(1))
 
 
 def test_get_performance_statistics(single_prices):
@@ -82,7 +83,7 @@ def test_get_performance_statistics(single_prices):
     stats = PerformanceStatistics().get_performance_statistics(single_prices, scale=s)
     ret = single_prices.iloc[-1] / single_prices.iloc[0]
     arc = ret ** (s / len(single_prices)) - 1
-    r = single_prices / single_prices.shift(1)
+    r = single_prices.diff(1) / single_prices.shift(1)
     asd = np.std(r) * np.sqrt(s)
     ir = arc / asd
     md = min((single_prices / single_prices.rolling(len(single_prices), min_periods=1).max() - 1).rolling(
@@ -101,10 +102,10 @@ def test_get_performance_statistics(single_prices):
     assert isinstance(stats['aRC'], float)
     assert 'IR2' in stats
     assert isinstance(stats['aRC'], float)
-    assert stats['IR2'] == ir * arc * np.sign(arc) / md
+    assert round(stats['IR2'], 10) == round(ir * arc * np.sign(arc) / md, 10)
     assert 'IR3' in stats
     assert isinstance(stats['aRC'], float)
-    assert stats['IR3'] == (arc ** 3) / (asd * md * mld)
+    assert round(stats['IR3'], 10) == round((arc ** 3) / (asd * md * mld), 10)
 
 
 def test_cumulative_returns(single_prices):